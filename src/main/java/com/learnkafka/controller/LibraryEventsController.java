beb41d0f3656e2aecaa7fe5acaa1bc78618575eb
Moustapha FALL <taphadjfall@yahoo.fr>
2024-09-18 16:11:23 -0400

    remove file and add config

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -30,7 +30,7 @@ public class LibraryEventsController {
         log.info("libraryEvent: {}", libraryEvent);
         //invoke the kafka producer
         //libraryEventsProducer.sendLibraryEvent(libraryEvent);
-        libraryEventsProducer.sendLibraryEvent_approach2(libraryEvent);
+        libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
         //libraryEventsProducer.sendLibraryEvent_approach3(libraryEvent);
         log.info("After sending libraryEvent: ");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
